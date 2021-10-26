1334bbcb1f8bdacc3b6591805ea00d75bef09d88
Ilya Shvabskiy <56001020+ilyadevwork@users.noreply.github.com>
2021-10-25 23:03:10 -0400

    Co-authored-by: Gary  <gchen1258@gmail.com>

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,46 @@
-console.log("Test");
+import axios from "axios";
+import UserAgent from "user-agents";
+import * as cheerio from "cheerio";
+import "url";
+
+// create client with timeout set
+const client = new axios.Axios({
+  timeout: 10000,
+});
+
+const userAgent = new UserAgent();
+
+const pullListings = async (s: URL): Promise<void> => {
+  try {
+    // use client created earlier to pull html
+    const { data } = await client.request({
+      method: "GET",
+      url: s.toString(),
+      headers: {
+        "user-agent": userAgent.toString(),
+      },
+    });
+
+    if (data) {
+      const listings: string[] = [];
+
+      const $ = cheerio.load(data);
+      $('div[id="mosaic-provider-jobcards"]')
+        .children("a")
+        .each((_, e) => {
+          const link = $(e).attr("href");
+          listings.push(`https://${s.hostname}${link}`);
+        });
+
+      console.log(listings.length);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const page = new URL(
+  "https://www.indeed.com/jobs?as_and=Software%20Engineer&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l=New%20York%2C%20NY&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=d30411f29f9d9423"
+);
+
+pullListings(page);
